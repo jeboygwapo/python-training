eed2a73d4774f36369c977b2ce2f88985b9c015c
jeboygwapo <jeboy.gearacam@gmail.com>
2026-03-24 18:33:22 +0300

    Day 4 up to video number 31

---
+++ Day4/mersenne_twister.py
@@ -0,0 +1,1 @@
+# Reference https://en.wikipedia.org/wiki/Mersenne_Twister
---
+++ Day4/project_rock_paper_scissors.py
@@ -0,0 +1,5 @@
+# Rock Paper Scissor Game using Randomization
+
+choice = input("What do you choose? "
+               "Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+
