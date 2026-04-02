fdd961a38ebca65b218559765ac412036b93b350
jeboygwapo <jeboy.gearacam@gmail.com>
2026-04-02 17:54:22 +0300

    initial push for project higher lower

---
+++ Day14/project_higherlower.py
@@ -0,0 +1,38 @@
+#from game_data import data
+from random import randint
+
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 183,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    }
+]
+
+# Printed strings 
+Compare A: {name}, a {description}, from {country}.
+vs
+Against B: {name}, a {description}, from {country}.
+input(Who has more followers? Type 'A' or 'B': )
+
+clear screen every after guess
+
+
+Once correct put the score above the Compare A 
+You're right! Current score: 1.
+
+Once you're wrong...
+Sorry, that's wrong. Final score 3
