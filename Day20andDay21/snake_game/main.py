452e6de01628b43137bf140584d840acdf227908
jeboygwapo <jeboy.gearacam@gmail.com>
2026-04-09 10:07:30 +0300

    squashed day 20 and 21 as it only have 1 project, snake game

--- Day20andDay21/snake_game/main.py
+++ Day20andDay21/snake_game/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
 import time
 
@@ -9,10 +9,18 @@ def initialize_screen():
     screen.bgcolor("black")
     screen.title("My Snake Game")
     screen.tracer(0)
+    return screen
 
 def main():
-    initialize_screen()
+    screen = initialize_screen()
     snake = Snake()
+
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+
     game_is_on = True
     while game_is_on:
         screen.update()
@@ -20,11 +28,7 @@ def main():
 
         snake.move()
 
-
+    screen.exitonclick()
 
 if __name__ == "__main__":
     main()
-
-
-screen.exitonclick()
-
