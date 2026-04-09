923cdab0ae358c30c2a47e6838b02ddbe1e9e3c9
jeboygwapo <jeboy.gearacam@gmail.com>
2026-04-09 12:26:34 +0300

    completed snake game Day21

--- Day20andDay21/snake_game/main.py
+++ Day20andDay21/snake_game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
+from food import Food
 from snake import Snake
+from scoreboard import Scoreboard
 import time
 
 def initialize_screen():
@@ -14,6 +16,8 @@ def initialize_screen():
 def main():
     screen = initialize_screen()
     snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
 
     screen.listen()
     screen.onkey(snake.up, "Up")
@@ -25,8 +29,25 @@ def main():
     while game_is_on:
         screen.update()
         time.sleep(0.1)
-
         snake.move()
+
+        # Detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
+
+        # Detect collision with wall
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+            game_is_on = False
+            scoreboard.game_over()
+
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
+
 
     screen.exitonclick()
 
